2c4c1076ab2d4e55188f3f2e5489e548fb595e12
Ritika <riyachhikara26@gmail.com>
2025-03-28 22:02:39 +0530

    Added a file.

---
+++ FibonacciSeries.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class FibonacciSeries {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the term upto which you want to print the fibonacci series: ");
+        int n = sc.nextInt();
+        int i = 1;
+        int n1 = 0;
+        int n2 = 1;
+        System.out.print(n1 + "  " + n2 + " ");
+        while (i <= n - 2) {
+            int n3 = n1 + n2;
+            System.out.print(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+            i++;
+        }
+    }
+}
