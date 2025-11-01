8dffdeecad3fa43f462ff18f53bc4b6e1a946769
Ritika <riyachhikara26@gmail.com>
2025-11-01 13:40:02 +0530

    Change

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -7,10 +7,10 @@ class FibonacciSeries {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the term upto which you want to print the fibonacci series: ");
         int n = sc.nextInt();
-        int i = 1;
         int n1 = 0;
         int n2 = 1;
         System.out.print(n1 + "  " + n2 + " ");
+        int i = 1;
         while (i <= n - 2) {
             int n3 = n1 + n2;
             System.out.print(n3 + " ");
